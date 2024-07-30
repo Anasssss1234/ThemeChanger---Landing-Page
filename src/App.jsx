f75ce84cb394d02e6ba6e942629c676aca4a1661
Anasssss1234 <anasq4628@gmail.com>
2024-07-30 11:20:28 +0530

    This is my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Page from './Components/Page'
-import { ThemeProvider } from './ThemeContext'
 
 function App() {
   return (
-    <>
-  <ThemeProvider>
-  <Page/>
-  </ThemeProvider>
-    </>
+    <div>
+      <Page/>
+    </div>
   )
 }
 
 export default App
+
--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -1,48 +1,62 @@
-import React, { useContext, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import '../App.css'
-import { ThemeContext } from '../ThemeContext';
-
 function Page() {
-    
+  const[city, setcity]=useState();
+  const [citychosen, setcityChosen]=useState(false);
+  const [cityName, setcityName]=useState({
+    name:'',
+    Temp:'',
+    descrip:'',
+  });
 
-    const {theme, moon, handleClick, Click} = useContext(ThemeContext);
-    return (
-        <>
-            <div className='background' style={{background:theme=='light'? 'white': '#1B1212', color:theme=='light'? 'black': '#ffff'}}>
-                <div className="card">
-                    <div className="header">
-                        <h2>Logo</h2>
-                        <div className="right">
-                            <ul>
-                                <li>{moon?(<i className="ri-sun-fill" onClick={handleClick}></i>):(<i className="ri-sun-line" onClick={Click}></i>)}</li>
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Contact</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="container">
-                        <div className="left-container">
-                            <div className="head">
-                                <h2>Hi, I am a Dangerous Animal</h2>
-                            </div>
-                            <div className="para">
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In eos dolorem placeat! Eum vel unde, commodi nisi placeat, itaque dignissimos libero a consectetur laudantium delectus officia nulla fugit sit aliquam?</p>
-                            </div>
-                            <div className="btn"><button>Animal</button></div>
-                        </div>
-                        <div className="right-container">
-                            <div className="image">
-                                <img src='https://cdn.pixabay.com/photo/2022/02/22/23/07/background-7029732_640.png' alt='Image' />
-                            </div>
-                        </div>
-                    </div>
+  const fetchData = async()=>{
+ try {
+   const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${`8389b92d5ff6122791e38e5a5a842d39`}`)
+   setcityName({
+    name:city,
+    Temp:result.data.main.temp,
+    descrip:result.data.weather[0].description,
+    coord:result.data.coord.lat,
+    coord1:result.data.coord.lon,
 
+   })
+ } catch (error) {
+    console.log("there is something error", error);
+ }
+ setcityChosen(true)
+  }
 
-                </div>
-            </div>
-        </>
-    )
+  const handleSearch = ()=>{
+    fetchData();
+  }
+
+  const handleChange = (e)=>{
+  setcity(e.target.value)
+  }
+  return (
+    <>
+    <div className='background'>
+      <div className="container">
+        <h1>Weather App</h1>
+        <div className="input">
+          <input type='text' placeholder='Enter City' value={city} onChange={handleChange}/>
+          <button onClick={handleSearch}>Search</button>
+          {!citychosen? (<></>):(<>
+          <div className='info'>
+            <h3>{cityName.name}</h3>
+            <h3>{cityName.Temp}</h3>
+            <h3>lat:{cityName.coord}</h3>
+            <h3>lon:{cityName.coord1}</h3>
+            <h3>{cityName.descrip}</h3>
+          </div>
+          </>)
+          }
+        </div>
+      </div>
+    </div>
+    </>
+  )
 }
 
-export default Page
+export default Page;
